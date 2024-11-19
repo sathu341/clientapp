59d890c6e387096eb9caecbf391845a01916d28e
sathu341 <sthishchandran321@gmail.com>
2024-11-19 08:43:41 +0530

    thrid commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
@@ -9,11 +10,16 @@ function App() {
   const handleSubmit=(e)=>{
     e.preventDefault()
     console.log(user)
+    axios.post("https://backendappl.vercel.app/",user)
+    .then((res)=>{
+        alert(res.data)
+    })
+    .catch(err=>console.log(err))
   }
 
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" name="user" onChange={(e)=>{
                 setUser(e.target.value)
         }} placeholder='enter your name'/>
